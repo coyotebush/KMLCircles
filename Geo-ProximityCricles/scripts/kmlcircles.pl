628fab473c4230968008735ee882ce17da980f23
Corey <coyotebush22@gmail.com>
2010-03-26 20:49:02 -0700

    POD

--- Geo-ProximityCricles/scripts/kmlcircles.pl
+++ Geo-ProximityCricles/scripts/kmlcircles.pl
@@ -32,3 +32,42 @@ print $kml $circles->kml;
 close($kml);
 
 __END__
+
+=head1 SYNOPSIS
+
+kmlcircles [-c color] [-r radius] outfile infile1 [infile2...]
+
+=head1 DESCRIPTION
+
+Given a set of waypoints contained in GPX files, a Google Earth file can be
+written containing circles that surround said waypoints. This is useful to
+visualize proximity restrictions, for instance in geocaching
+(L<http://www.geocaching.com/>).
+
+=head1 OPTIONS
+
+=over
+
+=item -c color
+
+Color for the circle fill and border, expressed in RGB using 6 hexadecimal
+digits. Default: ff0000 (red).
+
+=item -r radius
+
+Radius for the circles, in miles. Default: 0.1.
+
+=back
+
+=head1 ARGUMENTS
+
+The first argument is taken as a filename to which KML output will be written.
+
+Any other arguments are assumed to be GPX files containing the desired
+waypoints.
+
+=head1 SEE ALSO
+
+L<Geo::GoogleEarth::ProximityCircles>
+
+=cut
